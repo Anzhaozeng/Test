ab1d8eb71fe80bcb31829cb61e3c90c7976c2408
Layman <1159605595@qq.com>
2020-07-14 11:05:41 +0800

    first commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <windows.h>
+int main()
+{
+    int m,n=0;
+    printf("Hello World\n");
+    system("pause");
+    return 0;
+}
---
+++ 重复数字.cpp
@@ -0,0 +1,6 @@
+#include<vector>;
+class Solution {
+public:
+    int findRepeatNumber(vector<int>& nums) {
+    }
+};
